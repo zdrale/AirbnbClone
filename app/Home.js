2cd5f80101dc482b15b3407fc118535dbae54e17
zdrale <nikolazd22@outlook.com>
2023-06-20 21:15:05 +0200

    Fixed issue for get static props

---
+++ app/Home.js
@@ -0,0 +1,92 @@
+import Banner from "@/components/Banner";
+import Header from "@/components/Header";
+import SmallCard from "@/components/SmallCard";
+import MediumCard from "@/components/MediumCard";
+import LargeCard from "@/components/LargeCard";
+import Footer from "@/components/Footer";
+export default function Home({ exploreData, cardData }) {
+  const data = [
+    {
+      img: "https://links.papareact.com/5j2",
+      location: "London",
+      distance: "45-minute drive",
+    },
+    {
+      img: "https://links.papareact.com/1to",
+      location: "Manchester",
+      distance: "4.5-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/40m",
+      location: "Liverpool",
+      distance: "4.5-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/msp",
+      location: "York",
+      distance: "4-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/2k3",
+      location: "Cardiff",
+      distance: "45-minute drive",
+    },
+    {
+      img: "https://links.papareact.com/ynx",
+      location: "Birkenhead",
+      distance: "4.5-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/kji",
+      location: "Newquay",
+      distance: "6-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/41m",
+      location: "Hove",
+      distance: "2-hour drive",
+    },
+  ];
+  const data2 = [
+    { img: "https://links.papareact.com/2io", title: "Outdoor getaways" },
+    { img: "https://links.papareact.com/q7j", title: "Unique stays" },
+    { img: "https://links.papareact.com/s03", title: "Entire homes" },
+    { img: "https://links.papareact.com/8ix", title: "Pet allowed" },
+  ];
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {data?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={location}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {data2.map(({ img, title, location }) => (
+              <MediumCard key={location} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Whishlist Curated by Airbnb."
+          buttonText="Get Inspired"
+        />
+      </main>
+      <Footer />
+    </div>
+  );
+}
