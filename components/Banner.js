52f7c7148a425f12213c29c59e8a80f1b0a6cf35
zdrale <nikolazd22@outlook.com>
2023-06-20 20:46:21 +0200

    day one work

---
+++ components/Banner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+function Banner() {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+      <Image
+        fill
+        style={{ objectFit: "cover" }}
+        src="https://links.papareact.com/0fm"
+        alt="banner"
+      />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition durtaion-150">
+          I am flexlible
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 text-center mt-10">
+      <div className="space-y-4 text-xs text-grey-800">
+        <h5 className="font-bold">About</h5>
+        <p>How it works?</p>
+        <p>NewsRoom</p>
+        <p>Investors</p>
+        <p>Airnbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-grey-800">
+        <h5 className="font-bold">Community</h5>
+        <p>How it works?</p>
+        <p>NewsRoom</p>
+        <p>Investors</p>
+        <p>Airnbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-grey-800">
+        <h5 className="font-bold">Host</h5>
+        <p>How it works?</p>
+        <p>NewsRoom</p>
+        <p>Investors</p>
+        <p>Airnbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-grey-800">
+        <h5 className="font-bold">About</h5>
+        <p>How it works?</p>
+        <p>NewsRoom</p>
+        <p>Investors</p>
+        <p>Airnbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
